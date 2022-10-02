32004d83f685b4cb991d17769c37a065a493ddb8
Hazumiran <diazazhari32@gmail.com>
2022-10-02 23:15:31 +0700

    week 6 Object Oriented Programming

---
+++ src/Task2/TestShape.java
@@ -0,0 +1,14 @@
+package Task2;
+
+public class TestShape {
+		public static void main(String[] args) {
+		Shape s1 = new Shape("green", false);
+		System.out.println("Shape: " + s1.toString());		
+		Circle c1 = new Circle();
+		System.out.println("Circle: " + c1.toString());		
+		Rectangle r1 = new Rectangle();
+		System.out.println("Rectangle: " + r1.toString());		
+		Square s2 = new Square(10.0);
+		System.out.println("Square: " + s2.toString());
+	}
+}
